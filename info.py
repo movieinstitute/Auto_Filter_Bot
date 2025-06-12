4d5c17c409b568d7c86986a6e86217832c9ac3b5
movieinstitute <singhasarkarmrinmoy@gmail.com>
2025-06-12 13:01:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -160,7 +160,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'handicapped-shandee-moviesarkar-10c65f08.koyeb.app/))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
