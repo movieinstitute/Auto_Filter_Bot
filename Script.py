c85810a66d35c32967ebe0c13f76281872f8e2ab
movieinstitute <singhasarkarmrinmoy@gmail.com>
2025-06-12 23:05:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamcinezone">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/dreamcinezone>[Dreamcine_Botz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/movie_institute">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/movie_institute>[@Movie_institute_bot]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
