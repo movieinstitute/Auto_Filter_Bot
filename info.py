3943940708384403fe2b8d1a5b4efbdd48352e58
movieinstitute <singhasarkarmrinmoy@gmail.com>
2025-06-13 02:48:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'mrinmoysarka3639@naviaxis')
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('mongodb+srv://singhasarkarmrinmoy:<4IkS90QyhSdje7co>@cluster0.waiownw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://mrinmoysinghasarkar01:d0an0C27mvuBqpq5@cluster0.k8619.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://singhasarkarmrinmoy:<4IkS90QyhSdje7co>@cluster0.waiownw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files')
 
